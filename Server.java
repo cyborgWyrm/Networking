b8a7c43096e1e2c95c35a14fca15112a5b236861
cyborgWyrm <esorRevurg@gmail.com>
2024-11-29 11:05:28 -0700

    working on server

--- Server.java
+++ Server.java
@@ -1,1 +1,35 @@
-ds
+
+
+public class Server {
+	
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
+
+	private final int portNumber = 12345;
+	private final int backlogLimit = 100;
+	
+	
+	public void runServer() {
+		
+		try {
+			
+			server = new ServerSocket(portNumber, backlogLimit);
+			while () {
+				
+				
+			}
+			
+		}
+		catch {
+			
+			
+			
+		}
+		
+		
+	}
+	
+	
+}
